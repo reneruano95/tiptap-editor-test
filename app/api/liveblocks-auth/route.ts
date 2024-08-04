d14ebf98327f1b1ff73fd463fc2b62320ad98d26
Rene Ruano <rene.ruano95@gmail.com>
2024-08-03 21:02:34 -0400

    chore: update Liveblocks API key variable name

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -2,7 +2,7 @@ import { Liveblocks } from "@liveblocks/node";
 import { NextRequest } from "next/server";
 
 const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_API_KEY!,
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: NextRequest) {
