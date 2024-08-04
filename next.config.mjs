89961c396d0f40d67825c9eeb3afeda3b2b41613
Rene Ruano <rene.ruano95@gmail.com>
2024-08-04 10:07:37 -0400

    chore(config): update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: true,
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
 
